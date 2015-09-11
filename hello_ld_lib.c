45d4da28db0992ddbd4f8016a853383544d53ac7
argon1024 <argon1024@mail.ru>
2015-09-11 07:09:17 -0700

    Dobavlen zapusk funkcii cherez dlsym()

---
+++ hello_ld_lib.c
@@ -0,0 +1,29 @@
+
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "hello_lib.h"
+typedef int (*my_func)( char* );
+int main( int argc, char *argv[] ) {
+char *messg = "Hello world\n";
+   // Открываем совместно используемую библиотеку
+   void *dl_handle = dlopen( "./libhello.so", RTLD_LAZY );
+   if( !dl_handle ) {
+      printf( "ERROR open: %s\n", dlerror() );
+      return 3;
+   }
+   //  Находим адрес функции в библиотеке
+   my_func func = dlsym( dl_handle, "put_my_msg" );
+   char *error = dlerror();
+   if( error != NULL ) {
+      printf( "ERROR address: %s\n", dlerror() );
+      return 4;
+   }
+   // Вызываем функцию по найденному адресу
+   int res = (*func)( messg );
+   // Закрываем библиотеку
+   dlclose( dl_handle );
+   return res;
+}
+
+
