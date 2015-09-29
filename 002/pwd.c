1380ff6c49c13be094d4f67cbc68ff100d8091d3
argon1024 <argon1024@mail.ru>
2015-09-29 15:22:56 +0300

    29.09.15

--- 002/pwd.c
+++ 002/pwd.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+char pwd_string[] = {"PWD="};
+
 int main(int argc, char **argv, char **envp)
 {
-	if(envp == NULL) return -1;
-	while(*envp++){
-		char *p = strstr(*envp, "PWD=");
+	if(envp == NULL) return 1;
+	else             printf("**envp = %p\n", envp);
+	while(*envp){
+		char *p = strstr(*envp, pwd_string);
 		if(*envp == p){
-			printf("\ncurrent path: %s\n",p+4);
+			printf("\ncurrent path: %s\n",p + sizeof(pwd_string));
 			break;
 		}
+		*envp++;
 	}
 	return 0;
 }
