245ac4132665d82268bae5d36f9ce2d098596105
argon1024 <argon1024@mail.ru>
2015-10-09 14:59:06 +0300

    Repair 009 program

--- 009/pipe_readers.c
+++ 009/pipe_readers.c
@@ -3,6 +3,7 @@
 
 
 char buf[256];
+char buf2[256];
 
 int main(int argc, char** argv, char** env)
 {
@@ -14,13 +15,21 @@ int main(int argc, char** argv, char** env)
 	fd = popen(argv[2],"r");
 	if(fgets(buf, sizeof(buf), fd) == NULL) puts("Error! len string out of buf");
 	len = strlen(buf);
-	printf("\nString:%s\nlen=%d", buf, len);
+	printf("\nString:%s\nlen=%d\n", buf, len);
+#if 0
 	for(i=0; i < len; i++){
 		temp_char = buf[i];
 		buf[i] = buf[len - i -1];
 		buf[len-i-1] = temp_char;
 	}
 	printf("\nReversed string:%s", buf);
+#else
+	for(i = 0; i < len; i++) {
+		buf2[i] = buf[len - i -2];
+	}
+	buf2[i] = 0;
+	printf("\nReversed string:%s", buf2);
+#endif
 	puts("\n");
 	return 0;
 }
